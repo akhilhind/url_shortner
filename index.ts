74770653467a7b93c84f3da1cd4fc774c5d87691
Akhil <akhil@nagarro.com>
2023-02-24 19:08:49 +0530

    added index.html

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 const PORT = 4554;
+app.use(cors())
 
 app.get('/', async(req, res) => {
     console.log('incomming request');
